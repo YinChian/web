8f0a893b4c01e41ef664694cef295ee73be6cab5
ad143142 <1546298@gmail.com>
2022-11-02 18:30:56 +0800

    Main

---
+++ my_chat/asd.bat
@@ -0,0 +1,2 @@
+http-server -p 1546
+PAUSE
---
+++ my_chat/js/lobby.js
@@ -0,0 +1,22 @@
+let form = document.getElementById('lobby__form')
+
+let displaName = sessionStorage.getItem('display_name')
+if (displaName) {
+    form.name.value = displaName
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    sessionStorage.setItem('display_name', e.target.name.value)
+
+    let inviteCode = e.target.room.value
+    if (!inviteCode) [
+        inviteCode = String(Math.floor(Math.random() * 10000))
+    ]
+
+    window.location = `room.html?room=${inviteCode}`;
+
+})
+
+form.name.focus();
